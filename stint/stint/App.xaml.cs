6aee63a8bf15a42cb6ec953243bcd68fa6352608
Sahin <sahinyanlik@gmail.com>
2015-12-14 22:22:25 +0800

    Application converted to xaml

---
+++ stint/stint/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace stint
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent ();
+
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            XAlign = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+	}
+}
